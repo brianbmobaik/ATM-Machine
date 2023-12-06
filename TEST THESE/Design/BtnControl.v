c53e8cadf44ae3cee85c49f1d346dfd8bc96fb20
Brian Baik <brianbaik69@gmail.com>
2023-12-06 12:57:59 -0800

    Fixed

--- TEST THESE/Design/BtnControl.v
+++ TEST THESE/Design/BtnControl.v
@@ -3,49 +3,39 @@
 module BtnControl (
     input clk, rst,
     input [4:0] btn_i,
-    output reg [31:0] data_o
+    output [31:0] data_o
     );
     
     wire [4:0] btn_db;
-    wire [31:0] data;
     
     reg [2:0] sel;
-    reg [31:0] data_i;
+    reg [3:0] data [0:7];
+    integer i;
     
     BtnDebounce btn (.clk(clk), .rst(rst), .btn_i(btn_i), .btn_o(btn_db));
     
     always @ ( posedge |btn_db or posedge rst ) begin
         if ( rst ) begin
             sel <= 0;
-            data_i <= 0;
-        end else begin 
+            for(i = 0; i < 8; i = i + 1) begin
+                data[i] <= 0;
+            end
+        end else begin
             if ( btn_db[0] ) begin
                 sel <= sel == 7 ? 0 : sel + 1;
             end else if ( btn_db[1] ) begin
                 sel <= sel == 0 ? 7 : sel - 1;
             end else if ( btn_db[2] ) begin
-                data_i <= (data_i >= 9) ? 9 : data_i + 1;
+                data[sel] <= (data[sel] >= 9) ? 9 : data[sel] + 1;
             end else if ( btn_db[3] ) begin
-                data_i <= (data_i <= 0 ) ? 0 : data_i - 1;
+                data[sel] <= (data[sel] <= 0 ) ? 0 : data[sel] - 1;
             end else begin
                 sel <= sel;
-                data_i <= data_i;
+                data[sel] <= data[sel];
             end
         end
     end    
     
-    assign data = data_i >> (4 * sel);
-    
-    always @ ( * ) begin
-        case ( sel )
-            3'b000 : data_o <= {data_i[31: 4], data[3:0]};
-            3'b001 : data_o <= {data_i[31: 8], data[3:0], data_i[ 3:0]};
-            3'b010 : data_o <= {data_i[31:12], data[3:0], data_i[ 7:0]};
-            3'b011 : data_o <= {data_i[31:16], data[3:0], data_i[11:0]};
-            3'b100 : data_o <= {data_i[31:20], data[3:0], data_i[15:0]};
-            3'b101 : data_o <= {data_i[31:24], data[3:0], data_i[19:0]};
-            3'b110 : data_o <= {data_i[31:27], data[3:0], data_i[23:0]};
-            3'b111 : data_o <= {data[3:0], data_i[27:0]};
-        endcase
-    end
+    assign data_o = {data[7], data[6], data[5], data[4],
+                     data[3], data[2], data[1], data[0]};
 endmodule
