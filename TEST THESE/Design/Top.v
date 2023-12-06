a508f75b7f01906f7a8d64f05f0c01f1ce41fe41
Brian Baik <brianbaik69@gmail.com>
2023-12-06 11:20:13 -0800

    Fixed

--- TEST THESE/Design/Top.v
+++ TEST THESE/Design/Top.v
@@ -9,12 +9,11 @@ module Top(
     );
     
     wire [31:0] btn_data;
-    wire [2:0] btn_sel;
     wire sclk_1ms, sclk_250ms;
     
     ClkDivider #(.MAX_COUNT(49999)) clk_1ms (.clk(clk), .rst(rst), .sclk(sclk_1ms));
     ClkDivider #(.MAX_COUNT(24999999)) clk_250ms (.clk(clk), .rst(rst), .sclk(sclk_250ms));
-    BtnControl BtnCtrl (.clk(sclk_250ms), .rst(rst), .btn_i(btn), .data_o(btn_data), .sel_o(btn_sel));
+    BtnControl BtnCtrl (.clk(sclk_250ms), .rst(rst), .btn_i(btn), .data_o(btn_data));
     SevSegDisplay SSD (.sclk_1ms(sclk_1ms), .rst(rst), .data(btn_data), .LED_Out(LED_out), .LED_Control(LED_ctrl));
     
 //    always @ ( posedge rst or posedge clk ) begin
