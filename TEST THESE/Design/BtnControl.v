827dfbcdcfcc6b42c1831cf4544ed9db953bdea9
Brian Baik <brianbaik69@gmail.com>
2023-12-06 11:06:50 -0800

    Fixed

--- TEST THESE/Design/BtnControl.v
+++ TEST THESE/Design/BtnControl.v
@@ -3,35 +3,49 @@
 module BtnControl (
     input clk, rst,
     input [4:0] btn_i,
-    output reg [31:0] data_o,
-    output reg [2:0] sel_o
+    output reg [31:0] data_o
     );
     
-    wire [2:0] sel;
     wire [4:0] btn_db;
-    wire [3:0] LRtoUD, data;
-    reg [31:0] data_i = 0;
+    wire [31:0] data;
+    
+    reg [2:0] sel;
+    reg [31:0] data_i;
     
     BtnDebounce btn (.clk(clk), .rst(rst), .btn_i(btn_i), .btn_o(btn_db));
-    BtnLeftRight btnLR (.rst(rst), .btn(btn_db), .data_i(data_i),
-                        .sel_o(sel), .data_o(LRtoUD));
-    BtnUpDown btnUD (.rst(rst), .btn(btn_db), .data_i(LRtoUD), .data_o(data));
     
-    always @ ( posedge clk ) begin
+    always @ ( posedge |btn_db or posedge rst ) begin
+        if ( rst ) begin
+            sel <= 0;
+            data_i <= 0;
+        end else begin 
+            if ( btn_db[0] ) begin
+                sel <= sel == 7 ? 0 : sel + 1;
+            end else if ( btn_db[1] ) begin
+                sel <= sel == 0 ? 7 : sel - 1;
+            end else if ( btn_db[2] ) begin
+                data_i <= (data_i >= 9) ? 9 : data_i + 1;
+            end else if ( btn_db[3] ) begin
+                data_i <= (data_i <= 0 ) ? 0 : data_i - 1;
+            end else begin
+                sel <= sel;
+                data_i <= data_i;
+            end
+        end
+    end    
+    
+    assign data = data_i >> (4 * sel);
+    
+    always @ ( * ) begin
         case ( sel )
-            3'b000 : data_i[ 3: 0] <= data;
-            3'b001 : data_i[ 7: 4] <= data;
-            3'b010 : data_i[11: 8] <= data;
-            3'b011 : data_i[15:12] <= data;
-            3'b100 : data_i[19:16] <= data;
-            3'b101 : data_i[23:20] <= data;
-            3'b110 : data_i[27:24] <= data;
-            3'b111 : data_i[31:28] <= data;
+            3'b000 : data_o[ 3: 0] <= data[3:0];
+            3'b001 : data_o[ 7: 4] <= data[3:0];
+            3'b010 : data_o[11: 8] <= data[3:0];
+            3'b011 : data_o[15:12] <= data[3:0];
+            3'b100 : data_o[19:16] <= data[3:0];
+            3'b101 : data_o[23:20] <= data[3:0];
+            3'b110 : data_o[27:24] <= data[3:0];
+            3'b111 : data_o[31:28] <= data[3:0];
         endcase
-    end
-    
-    always @ ( data_i or sel ) begin
-        data_o <= data_i;
-        sel_o <= sel;
     end
 endmodule
